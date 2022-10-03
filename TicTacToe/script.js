3a74af6aa3f349e5d79ab5b4628c137ff9d40967
mithileshjoshi100 <40782595+mithileshjoshi100@users.noreply.github.com>
2022-10-03 09:59:44 +0530

    css and reset btn added

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,3 +1,6 @@
+const p1 = '🐥';
+const p2 = '🐝';
+const blank = '';
 let player = 1;
 let b1, b2, b3, b4, b5, b6, b7, b8, b9;
 b1 = document.getElementById("b1");
@@ -41,15 +44,15 @@ b9.addEventListener("click", function () {
 });
 
 function boxClick(x) {
-    if (x.innerHTML != '0') {
+    if (x.innerHTML != blank) {
         return;
     }
     else if (player === 1) {
-        x.innerHTML = '1';
+        x.innerHTML = p1;
         player = 2;
     }
     else if (player == 2) {
-        x.innerHTML = '-1';
+        x.innerHTML = p2;
         player = 1;
     }
     else {
@@ -60,15 +63,15 @@ function boxClick(x) {
 
 function check() {
     // horizontal lines
-    if (b1.innerHTML != '0' && areEqual(b1.innerHTML, b2.innerHTML, b3.innerHTML)) {
-        if (b1.innerHTML == '1') {
+    if (b1.innerHTML != blank && areEqual(b1.innerHTML, b2.innerHTML, b3.innerHTML)) {
+        if (b1.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b1.innerHTML == '-1') {
+        else if (b1.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -76,15 +79,15 @@ function check() {
         }
     }
 
-    if (b4.innerHTML != '0' && areEqual(b4.innerHTML, b5.innerHTML, b6.innerHTML)) {
-        if (b4.innerHTML == '1') {
+    if (b4.innerHTML != blank && areEqual(b4.innerHTML, b5.innerHTML, b6.innerHTML)) {
+        if (b4.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b4.innerHTML == '-1') {
+        else if (b4.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -92,15 +95,15 @@ function check() {
         }
     }
 
-    if (b7.innerHTML != '0' && areEqual(b7.innerHTML, b8.innerHTML, b9.innerHTML)) {
-        if (b7.innerHTML == '1') {
+    if (b7.innerHTML != blank && areEqual(b7.innerHTML, b8.innerHTML, b9.innerHTML)) {
+        if (b7.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b7.innerHTML == '-1') {
+        else if (b7.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -109,15 +112,15 @@ function check() {
     }
 
     // vertical lines
-    if (b1.innerHTML != '0' && areEqual(b1.innerHTML, b4.innerHTML, b7.innerHTML)) {
-        if (b1.innerHTML == '1') {
+    if (b1.innerHTML != blank && areEqual(b1.innerHTML, b4.innerHTML, b7.innerHTML)) {
+        if (b1.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b1.innerHTML == '-1') {
+        else if (b1.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -125,15 +128,15 @@ function check() {
         }
     }
 
-    if (b2.innerHTML != '0' && areEqual(b2.innerHTML, b5.innerHTML, b8.innerHTML)) {
-        if (b2.innerHTML == '1') {
+    if (b2.innerHTML != blank && areEqual(b2.innerHTML, b5.innerHTML, b8.innerHTML)) {
+        if (b2.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b2.innerHTML == '-1') {
+        else if (b2.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -141,15 +144,15 @@ function check() {
         }
     }
 
-    if (b3.innerHTML != '0' && areEqual(b3.innerHTML, b6.innerHTML, b9.innerHTML)) {
-        if (b3.innerHTML == '1') {
+    if (b3.innerHTML != blank && areEqual(b3.innerHTML, b6.innerHTML, b9.innerHTML)) {
+        if (b3.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b3.innerHTML == '-1') {
+        else if (b3.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -159,15 +162,15 @@ function check() {
 
     // cross lines
 
-    if (b1.innerHTML != '0' && areEqual(b1.innerHTML, b5.innerHTML, b9.innerHTML)) {
-        if (b1.innerHTML == '1') {
+    if (b1.innerHTML != blank && areEqual(b1.innerHTML, b5.innerHTML, b9.innerHTML)) {
+        if (b1.innerHTML == p1) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b1.innerHTML == '-1') {
+        else if (b1.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -175,15 +178,15 @@ function check() {
         }
     }
 
-    if (b3.innerHTML != '0' && areEqual(b3.innerHTML, b5.innerHTML, b7.innerHTML)) {
-        if (b3.innerHTML == '1') {
+    if (b3.innerHTML != blank && areEqual(b3.innerHTML, b5.innerHTML, b7.innerHTML)) {
+        if (b3.innerHTML == p2) {
             console.log('Player 1 Wins');
-            alert('Player 1 Wins');
+            alert(`Player ${p1} Wins`);
             reset();
         }
-        else if (b3.innerHTML == '-1') {
+        else if (b3.innerHTML == p2) {
             console.log('Player 2 wins');
-            alert('Player 2 Wins');
+            alert(`Player ${p2} Wins`);
             reset();
         }
         else {
@@ -196,7 +199,7 @@ function check() {
 function reset() {
     for (let row of matrix) {
         for (let element of row) {
-            element.innerHTML = '0';
+            element.innerHTML = '';
         }
     }
 }
